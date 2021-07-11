a7f06c2e148593a8cfe419bdb345cddb0ad537e4
wheejoo <53321214+wheejoo@users.noreply.github.com>
2021-07-11 17:27:38 +0900

    Update 나이트이동_김휘주.py

--- 1주차 BFS,DFS/나이트의 이동/나이트이동_김휘주.py
+++ 1주차 BFS,DFS/나이트의 이동/나이트이동_김휘주.py
@@ -7,14 +7,14 @@ def bfs(x1,y1,x2,y2):
     q.append((x1,y1))
     while q:
         x,y = q.popleft()
-        if x == x2 and y == y2:
-            print(graph[x2][y2])
+        if x == x2 and y == y2: #끝좌표와 같다면
+            print(graph[x2][y2]) #출력
             break
         for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < l and 0<= ny < l:
-                if graph[nx][ny] == 0: #끝좌표가 0이라면
+                if graph[nx][ny] == 0: 
                     q.append((nx, ny))
                     graph[nx][ny] = graph[x][y]+1
 
