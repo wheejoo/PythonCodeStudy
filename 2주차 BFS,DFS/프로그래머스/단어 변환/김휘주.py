a1e982c2dde69c1ef75056b28f1fa15b4bb1ad46
wheejoo <53321214+wheejoo@users.noreply.github.com>
2021-07-17 18:55:37 +0900

    Update 김휘주.py

--- 2주차 BFS,DFS/프로그래머스/단어 변환/김휘주.py
+++ 2주차 BFS,DFS/프로그래머스/단어 변환/김휘주.py
@@ -23,4 +23,8 @@ begin = "hit"
 target = "cog"
 words = ["hot", "dot", "dog", "lot", "log", "cog"]
 
+# begin = "hit"	
+# target = "cog"	
+# words = ["hot", "dot", "dog", "lot", "log"]
+
 print(solution(begin, target, words))
